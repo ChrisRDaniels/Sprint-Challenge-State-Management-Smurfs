7155c80e823b4da8ee113db49ed1c42e22586dc6
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-11-08 09:29:59 -0700

    intial commit with server listening/dependencies installed

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const SUBMIT_FORM = "SUBMIT_FORM";
+export const START_FETCHING = "START_FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_ERROR = "FETCH_ERROR";
+
